289e1aa1d3c36d9fce8003750c5b588c3eab9d95
Junya Kono <junya03dance@gmail.com>
2017-08-04 11:49:58 +0900

    ハンズオンようにセットアップ

---
+++ README.md
@@ -0,0 +1,8 @@
+# react-starter-kit
+Reactを始めるのにとりあえず環境だけ作っているもの。
+
+```
+$ git clone git@github.com:konojunya/react-starter-kit.git
+$ cd react-starter-kit
+$ npm i
+```
---
+++ src/app.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { render } from "react-dom";
+
+// componentをimportする↓
+
+render(
+    <Hello/>,
+    document.getElementById("app")
+);
